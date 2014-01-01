20cf1bf1d914a84787a16b4af9286dda94038a25
Robin Fisher <robinjfisher@gmail.com>
2014-01-01 14:47:09 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Budgeteer::Application.config.secret_key_base = '9e797880177d1202e530f06c053d63203a047970c4f1c777573f5d4088481658c95ddabad45dcd3b7fbc36c9909b0efe61edf4cbc328a8b13a854f8e899280f6'
